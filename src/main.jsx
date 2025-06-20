fdcef6ddf69e29bc94ccd734a04c1bb5bda6b27e
Lior30 <lior8712@gmail.com>
2025-06-20 06:47:59 +0300

    fixed the privateRoutes bug that returned to landing page after refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-
-// src/main.jsx
 import "leaflet/dist/leaflet.css";
 import "leaflet-geosearch/dist/geosearch.css";
 import React, { useState } from "react";
@@ -9,28 +7,29 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { getDesignTheme } from "./theme.js";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthProvider } from './context/AuthProvider.jsx';
 
 function Root() {
-  // מצב התצוגה: light או dark
   const [mode, setMode] = useState("light");
 
-  // Toggle בין המצבים
   const toggleTheme = () => {
     setMode((prev) => (prev === "light" ? "dark" : "light"));
   };
 
-  // בונים את ה-theme לפי המצב הנוכחי
   const theme = getDesignTheme(mode);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        {/* מעבירים את toggleTheme וה־mode ל־App */}
         <App toggleTheme={toggleTheme} mode={mode} />
       </BrowserRouter>
     </ThemeProvider>
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <Root />
+  </AuthProvider>
+);
